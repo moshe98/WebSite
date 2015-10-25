3f2b1aab6c79b6538ae9eabbd15fce5d7c6eab07
Moshe_b <bomberdugo@gmail.com>
2015-10-26 00:01:59 +0200

    Added excluded files

---
+++ WebSite.Services/Infrastructure/ILoging.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSite.Services.Infrastructure
+{
+    public interface ILoging
+    {
+        string LogFileName { get; set; }
+        StreamWriter StreamWriter { get; set; }
+
+        bool Open();
+        string Log(string message);
+        bool Close(string filename);
+    }
+}
