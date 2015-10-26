69b33b928e7ae5c0ab92058370ebf8e2d53aef31
Moshe_b <bomberdugo@gmail.com>
2015-10-26 22:46:21 +0200

    removing test

--- WebSite/Scripts/Tutorial.jsx
+++ WebSite/Scripts/Tutorial.jsx
@@ -1,5 +1,4 @@
-﻿//testing
-var data = [
+﻿var data = [
   { Author: "Daniel Lo Nigro", Text: "Hello ReactJS.NET World!" },
   { Author: "Pete Hunt", Text: "This is one comment" },
   { Author: "Jordan Walke", Text: "This is *another* comment" }
